c163c656a6849ffbc9b41e0c045cb797f1295afb
EmilioVejarDiaz <105244286+EmilioVejarDiaz@users.noreply.github.com>
2024-10-03 15:32:46 -0600

    Segundo Commit

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Moneda : MonoBehaviour
 {
+    public AudioSource moneda;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("GANASTEEEEEEEEE!!!!!!");
+            moneda.PlayOneShot(moneda.clip);
             Destroy(this.gameObject);
         }
     }
